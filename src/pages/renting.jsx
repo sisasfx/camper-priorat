1320879c97f14c91c87b81ddb8c8bebbb10efe21
Miquel Ortiz <miquel.ortiz.7e4@itb.cat>
2023-06-21 19:08:37 +0200

    feat:add renting title

--- src/pages/renting.jsx
+++ src/pages/renting.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import "../styles/pages.css";
 import Slider from "../components/slider.jsx";
 
-
-
 export const Renting = () => {
-  
-
   return (
     <div className="main-renting">
-      <h1>Welcome to Camper Priorat Renting</h1>
+      <div className="title">
+        <h1>Welcome to Camper Priorat Renting</h1>
+      </div>
       <div className="slide-container">
-        <Slider/>
+        <Slider />
       </div>
     </div>
   );
