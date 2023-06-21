605129cf84e5dcf88824e93d75aa98c9975d1d48
Miquel Ortiz <miquel.ortiz.7e4@itb.cat>
2023-06-21 13:09:38 +0200

    fix:Slider Images

--- src/pages/renting.jsx
+++ src/pages/renting.jsx
@@ -1,49 +1,19 @@
 import React from "react";
-import { Slide } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
 import "../styles/pages.css";
-import Renting1 from "../img/IMG-20230619-WA0012.jpg";
-import Renting2 from "../img/IMG-20230619-WA0013.jpg";
-import Renting3 from "../img/IMG-20230619-WA0014.jpg";
-import Renting4 from "../img/IMG-20230619-WA0015.jpg";
-import Renting5 from "../img/IMG-20230619-WA0016.jpg";
-import Renting6 from "../img/IMG-20230619-WA0017.jpg";
+import Slider from "../components/slider.jsx";
+
+
 
 export const Renting = () => {
-  const images = [Renting1, Renting2, Renting3, Renting4, Renting5, Renting6];
+  
 
   return (
-      <Slide>
-        <div className="each-slide">
-          <div style={{'backgroundImage': `url(${images[0]})` }}
-          ></div>
-        </div>
-        <div className="each-slide">
-          <div
-            style={{'backgroundImage': `url(${images[1]})` }}
-          ></div>
-        </div>
-        <div className="each-slide">
-          <div
-            style={{'backgroundImage': `url(${images[2]})` }}
-          ></div>
-        </div>
-        <div className="each-slide">
-          <div
-            style={{'backgroundImage': `url(${images[3]})` }}
-          ></div>
-        </div>
-        <div className="each-slide">
-          <div
-            style={{'backgroundImage': `url(${images[4]})` }}
-          ></div>
-        </div>
-        <div className="each-slide">
-          <div
-            style={{'backgroundImage': `url(${images[5]})` }}
-          ></div>
-        </div>
-      </Slide>
+    <div className="main-renting">
+      <h1>Welcome to Camper Priorat Renting</h1>
+      <div className="slide-container">
+        <Slider/>
+      </div>
+    </div>
   );
 };
 
