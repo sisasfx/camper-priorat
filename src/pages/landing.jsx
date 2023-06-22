b06215331dde8db3be7680ad65eae8e9bf7a6e81
JorgePG-L <jorgepiqueras92@gmail.com>
2023-06-22 23:59:54 +0200

    cambios

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -9,38 +9,32 @@ import "../styles/pages.css";
 export const Landing = () => {
   return (
     <main className="main-landing">
-      <section className="description-box-1">
-        <p className="description">
-          ¡Bienvenidos a Camper Priorat! Descubre la libertad de viajar con
-          nuestra furgoneta camperizada. Explora la naturaleza sin renunciar a
-          la comodidad de un hogar sobre ruedas.
-        </p>
-        <img className="landScape-3" alt="logo-landScape" src={landScape} />
-      </section>
+      <section className="description-box-1"></section>
 
       <section className="description-box-3">
-        <p className="description-3">
-        ¡Bienvenidos a Camper Priorat! Descubre la libertad de viajar con
-          nuestra furgoneta camperizada. Explora la naturaleza sin renunciar a
-          la comodidad de un hogar sobre ruedas.
+        <div className="image-container"></div>
+        <p className="description-3">¡Bienvenidos a Camper Priorat!</p>
+        <p className="description-4">
+          Descubre la libertad de viajar con nuestra furgoneta camperizada.
+          Explora la naturaleza sin renunciar a la comodidad de un hogar sobre
+          ruedas.
         </p>
-        
       </section>
 
       <section className="description-box-2">
-      <p className="activity-title">Tenemos lo que necesitas para que puedas hacer</p>
-       <div className="card-activity">
+        <p className="activity-title">Tenemos lo que necesitas para que puedas hacer</p>
+        <div className="card-activity">
           <img className="escalada" alt="escalada" src={escaladaPhoto} />
-          <img className="piragua" alt="piragua" src={piraguaPhoto}/>
-          <img className="sky" alt="sky" src={sky}/>
-       </div>
+          <img className="piragua" alt="piragua" src={piraguaPhoto} />
+          <img className="sky" alt="sky" src={sky} />
+        </div>
         <div className="indescription-2">
-          <img className="contact-card" alt="contact-card" src={contactCard}/>
+          <img className="contact-card" alt="contact-card" src={contactCard} />
           <p className="description-2">
             ¿Qué nos hace diferentes? La combinación perfecta de aventura y
             confort.
           </p>
-        </div>  
+        </div>
       </section>
     </main>
   );
