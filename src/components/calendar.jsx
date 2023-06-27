6ec5411ac70cc8b4787cf1c5c41416d6721060af
Miquel Ortiz <miquel.ortiz.7e4@itb.cat>
2023-06-27 16:39:08 +0200

    feat:dinamic calendar working on last days of prev month

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -1,19 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/components.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 export const Calendar = () => {
+    let date = new Date();
+    let currYear = date.getFullYear();
+    const months = ['January', 'February', 'March', 'April', 'May', 'June','July','August','Septembre', 'October','November','December'];
+    const days = [];
+    
+    const [month, setIntMonth] = useState(date.getMonth())
+    const [daysOfMonthState, setDays] = useState(days)
+    const [currentMonth, setCurrentMonth] = useState(months[month])
+    
+    let lastDateofMonth = new Date(currYear, month + 1,0).getDate();
+    
+    for(let i = 1; i <= lastDateofMonth; i++){
+        days.push(i)
+    }
+
+    
+    const changePlusMonth = () => {
+        setIntMonth(month+1)
+    }
+
+    const changeMinusMonth = () => {
+        setIntMonth(month-1)
+    }
+
+    useEffect(() => {
+        setCurrentMonth(months[month])
+        days.splice(0, days.length)
+        let lastDateofMonth = new Date(currYear, month + 1,0).getDate();
+    
+        for(let i = 1; i <= lastDateofMonth; i++){
+            days.push(i)
+        }
+
+        setDays(days)
+    },[month])
+
+
     return(
         <div className="calendar-box">
             <header className="calendar-header">
-                <p className="current-date">June 2023</p>
+                <p className="current-date">{currentMonth} {currYear}</p>
                 <div className="icons">
-                    <span className="arrow-left">
-                    <FontAwesomeIcon icon={faArrowLeft} />
+                    <span className="arrow-left" >
+                    <FontAwesomeIcon icon={faArrowLeft} onClick={changeMinusMonth} />
                     </span>
-                    <span className="arrow-right">
-                    <FontAwesomeIcon icon={faArrowRight} />
+                    <span className="arrow-right" >
+                    <FontAwesomeIcon icon={faArrowRight} onClick={changePlusMonth} />
                     </span>
                 </div>
             </header>
@@ -28,41 +65,9 @@ export const Calendar = () => {
                     <li>Sun</li>
                 </ul>
                 <ul className="days">
-                    <li className="inactive">29</li>
-                    <li className="inactive">30</li>
-                    <li className="inactive">31</li>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                    <li>4</li>
-                    <li>5</li>
-                    <li>6</li>
-                    <li>7</li>
-                    <li>8</li>
-                    <li>9</li>
-                    <li>10</li>
-                    <li>11</li>
-                    <li>12</li>
-                    <li>13</li>
-                    <li>14</li>
-                    <li>15</li>
-                    <li>16</li>
-                    <li>17</li>
-                    <li>18</li>
-                    <li>19</li>
-                    <li>20</li>
-                    <li>21</li>
-                    <li>22</li>
-                    <li>23</li>
-                    <li>24</li>
-                    <li>25</li>
-                    <li>26</li>
-                    <li>27</li>
-                    <li>28</li>
-                    <li>29</li>
-                    <li>30</li>
-                    <li className="inactive">1</li>
-                    <li className="inactive">2</li>
+                    {daysOfMonthState.map(
+                        (item, key) => <li alt={key}>{item}</li>
+                    )}
                 </ul>
             </div>
         </div>
