b31848f75b04b6d79a60932ca2f19e2b48e70c3b
Miquel Ortiz <miquel.ortiz.7e4@itb.cat>
2023-06-28 19:40:26 +0200

    feat:navbar responsive

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,26 +1,49 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import "../styles/components.css"
-import logo from "../img/logo_three.png"
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import "../styles/components.css";
+import logo from "../img/logo_three.png";
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 export const NavBar = () => {
-    return(
-        <nav className='navbar d-flex justify-content-start'>
-            <img className='logo-img' alt='logo'src={logo}/>
-            <ul className='item-list'>
-                <Link to={'/market'} style={{ textDecoration: 'none' }}>
-                    <li className='item-from-list'>Venta</li>
-                </Link>
-                <Link to={'/renting'} style={{ textDecoration: 'none' }}>
-                    <li className='item-from-list'>Alquiler</li>
-                </Link>
-                <Link to={'/'} style={{ textDecoration: 'none' }}>
-                    <li className='item-from-list'>Contacto</li>
-                </Link>                
-            </ul>
-        </nav>
-    );
-}
+
+  const [isOpen, setOpen] = useState(false)
+
+  return (
+    <div className="navs-container">
+      <nav className="navbar-desktop">
+        <img className="logo-img" alt="logo" src={logo} />
+        <ul className="item-list">
+          <Link to={"/market"} style={{ textDecoration: "none" }}>
+            <li className="item-from-list">Venta</li>
+          </Link>
+          <Link to={"/renting"} style={{ textDecoration: "none" }}>
+            <li className="item-from-list">Alquiler</li>
+          </Link>
+          <Link to={"/"} style={{ textDecoration: "none" }}>
+            <li className="item-from-list">Contacto</li>
+          </Link>
+        </ul>
+      </nav>
+      <nav className="navbar-phone">
+        <img className="logo-img" alt="logo" src={logo} />
+        <span className="burger-button">
+          <FontAwesomeIcon icon={faBars} onClick={() => setOpen(!isOpen)} />
+        </span>
+        <ul className={`item-list-phone ${isOpen ? "open": "notOpen"}`}>
+          <Link to={"/market"} style={{ textDecoration: "none" }}>
+            <li className="item-from-list-phone">Venta</li>
+          </Link>
+          <Link to={"/renting"} style={{ textDecoration: "none" }}>
+            <li className="item-from-list-phone">Alquiler</li>
+          </Link>
+          <Link to={"/"} style={{ textDecoration: "none" }}>
+            <li className="item-from-list-phone">Contacto</li>
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default NavBar;
