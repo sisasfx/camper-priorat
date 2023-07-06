969ef95c3fa3cdb8de159d2ef956e97bb0d81198
Miquel Ortiz <miquel.ortiz.7e4@itb.cat>
2023-07-06 11:32:42 +0200

    feat:renting almost done

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,37 +1,34 @@
-import React from "react";
-import { Slide } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
-import Renting1 from "../img/IMG-20230619-WA0012.jpg";
-import Renting2 from "../img/IMG-20230619-WA0013.jpg";
-import Renting3 from "../img/IMG-20230619-WA0014.jpg";
-import Renting4 from "../img/IMG-20230619-WA0015.jpg";
-import Renting5 from "../img/IMG-20230619-WA0016.jpg";
-import Renting6 from "../img/IMG-20230619-WA0017.jpg";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import "../styles/components.css"
+export const Slider = ({imagenes}) => {
 
-export const Slider = () => {
-  const images = [Renting1, Renting2, Renting3, Renting4, Renting5, Renting6];
-  return (
-      <Slide>
-        <div className="each-slide-effect">
-          <div style={{ backgroundImage: `url(${images[0]})` }}></div>
-        </div>
-        <div className="each-slide-effect">
-          <div style={{ backgroundImage: `url(${images[1]})` }}></div>
-        </div>
-        <div className="each-slide-effect">
-          <div style={{ backgroundImage: `url(${images[2]})` }}></div>
-        </div>
-        <div className="each-slide-effect">
-          <div style={{ backgroundImage: `url(${images[3]})` }}></div>
-        </div>
-        <div className="each-slide-effect">
-          <div style={{ backgroundImage: `url(${images[4]})` }}></div>
-        </div>
-        <div className="each-slide-effect">
-          <div style={{ backgroundImage: `url(${images[5]})` }}></div>
-        </div>
-      </Slide>
-  );
-};
+    const [currentImg, setImg] = useState(0);
+    const imageSize = imagenes?.length;
 
-export default Slider;
+    const nextImage = () => {
+        setImg(currentImg === imageSize -1 ? 0 : currentImg +1)
+    }
+
+    
+    const prevImage = () => {
+        setImg(currentImg === 0 ? imageSize -1 : currentImg -1)
+    }
+    return(
+        <div className="slider-container">
+            <button onClick={prevImage}>
+                <FontAwesomeIcon icon={faArrowLeft}/>
+            </button>
+            {imagenes.map((item, key)=> {
+                return( 
+                <div>
+                    {currentImg === key && (<img key={key} src={item} alt="imagen"/>)}                    
+                </div> )               
+            })}
+            <button onClick={nextImage}>
+                <FontAwesomeIcon icon={faArrowRight}/>
+            </button>
+        </div>
+    );
+}
--- src/pages/renting.jsx
+++ src/pages/renting.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "../styles/pages.css";
-import Slider from "../components/slider.jsx";
 import Calendar from "../components/calendar";
+import { Slider } from "../components/slider";
+import Renting1 from "../img/IMG-20230619-WA0012.jpg";
+import Renting2 from "../img/IMG-20230619-WA0013.jpg";
+import Renting3 from "../img/IMG-20230619-WA0014.jpg";
+import Renting4 from "../img/IMG-20230619-WA0015.jpg";
+import Renting5 from "../img/IMG-20230619-WA0016.jpg";
+import Renting6 from "../img/IMG-20230619-WA0017.jpg";
+
 
 export const Renting = () => {
+  const images = [Renting1, Renting2, Renting3, Renting4, Renting5, Renting6];
   return (
     <div className="main-renting">
       <div className="title">
@@ -12,6 +20,9 @@ export const Renting = () => {
       <section className="renting-section">
         <div className="calendar-container">
           <Calendar />
+        </div>
+        <div className="slider-container">
+          <Slider imagenes={images}/>
         </div>
       </section>
     </div>
