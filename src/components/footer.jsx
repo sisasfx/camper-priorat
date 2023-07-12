b89b8a7f3adb96591ab2eecc494c7adfd03d9dec
Miquel Ortiz <miquel.ortiz.7e4@itb.cat>
2023-07-12 21:37:05 +0200

    fix footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import footer from "../img/footer.png"
+import foot from "../img/footer.png"
 import "../styles/components.css"
 
 export const Footer = () => {
     return (
         <footer className="foot d-flex">
-            <img className="footer-page" alt="footer" src={footer}/>
+            <img className="footer-page" alt="footer" src={foot}/>
         </footer>
     );
 }
